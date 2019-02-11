d2a00cdee14d972eb22370b45486185c25520a23
Hanna Ismahilova <Hanna_Ismahilova@epam.com>
2019-02-11 17:17:04 +0100

    Java lesssons

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Math_1.java
@@ -0,0 +1,15 @@
+package com.mycompany.mavenproject1;
+
+/* 
+Выводим квадрат числа
+*/
+public class Math_1 
+{
+    public static int number = 25;
+
+    public static void main(String[] args) {
+        int result = number * number;
+        System.out.println(result);
+        
+    }
+}
---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Math_2.java
@@ -0,0 +1,12 @@
+package com.mycompany.mavenproject1;
+
+/* 
+На нашем экране — переменная
+*/
+public class Math_2  {
+    public static void main(String[] args) {
+        String name = "Hanna";
+        System.out.println(name);
+    }
+}
+
