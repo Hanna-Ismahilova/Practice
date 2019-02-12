8be5d14b31123f2c168aac93724f5a93a38b85bc
Hanna Ismahilova <Hanna_Ismahilova@epam.com>
2019-02-12 16:06:57 +0100

    A lot of mini java programms:)

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Zadanie2.java
@@ -0,0 +1,25 @@
+//Напиши программу, которая выводит на экран надпись: "JavaRush. Learn once - use anywhere" 10 раз.
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Hanna_Ismahilova
+ */
+public class Zadanie2 {
+    public static void main(String[] args) {
+        String text = "JavaRush. Learn once - use anywhere";
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        //напишите тут ваш код
+    }
+}
+
