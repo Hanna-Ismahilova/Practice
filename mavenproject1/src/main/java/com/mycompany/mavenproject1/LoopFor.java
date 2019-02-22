21a28939d665d2ecd28a8a12009b519a4f140402
Hanna Ismahilova <Hanna_Ismahilova@epam.com>
2019-02-22 15:46:53 +0100

    Solutions for new tasks

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/LoopFor.java
@@ -0,0 +1,15 @@
+/*
+Демонстрация применения цикла for
+ */
+package com.mycompany.mavenproject1;
+
+public class LoopFor {
+    public static void main(String[] args){
+     int count;
+     
+     for(count = 0; count < 5; count = count+1) //Этот цнкя выnоnняет 5 iterations
+         System.out.println("Знaчeниe счетчика: " + count);
+     System.out.println("Гoтoвo!");
+    }
+    
+}
