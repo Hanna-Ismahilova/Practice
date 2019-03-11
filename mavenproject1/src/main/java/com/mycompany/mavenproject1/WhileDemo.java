29583b8ac3ff52a25a41c85212bd6216be93a706
Hanna Ismahilova <Hanna_Ismahilova@epam.com>
2019-03-11 17:51:42 +0100

    Solution for the new tasks

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/WhileDemo.java
@@ -0,0 +1,16 @@
+package com.mycompany.mavenproject1;
+
+public class WhileDemo {
+	public static void main(String[] args) {
+		char ch;
+		
+		//вывести буквы английского алфавита, используя цикл while 
+		ch = 'a';
+		while (ch <= 'z') {
+			System.out.println(ch);
+			ch++;
+			
+		}
+	}
+
+}
