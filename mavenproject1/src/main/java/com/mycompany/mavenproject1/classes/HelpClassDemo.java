f4805ba854f7d2806eb24168042f83909f4bdf93
Hanna Ismahilova <hannaismahilova@gmail.com>
2019-05-03 21:02:56 +0200

    help class

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/classes/HelpClassDemo.java
@@ -0,0 +1,30 @@
+package com.mycompany.mavenproject1.classes;
+
+public class HelpClassDemo {
+    public static void main(String[] args) throws java.io.IOException {
+
+        char choice, ignore;
+        Help hlpodj = new Help();
+
+        for (;;) {
+            do {
+                hlpodj.showmenu();
+
+                choice = (char) System.in.read();
+
+
+                do {
+                    ignore = (char) System.in.read();
+                } while (ignore != '\n');
+            }while ( !hlpodj.isvalid(choice));
+
+            if (choice == 'q') break;
+
+            System.out.println("\n");
+
+            hlpodj.helpon(choice);
+        }
+
+    }
+
+}
