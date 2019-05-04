ce4a5d81d25acb1dcba9637496d7211324c33f27
Hanna Ismahilova <hannaismahilova@gmail.com>
2019-05-04 10:58:16 +0200

    constructor

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/classes/MyClassConstructorDemo.java
@@ -0,0 +1,17 @@
+package com.mycompany.mavenproject1.classes;
+
+public class MyClassConstructorDemo {
+    public static void main(String[] args) {
+
+        //Обращение к конктрутору без параметров
+//        MyClass objectConstr1 = new MyClass();
+//        MyClass objectConstr2 = new MyClass();
+//        System.out.println(objectConstr1.x + " " + objectConstr2.x);
+
+        //Обращение к конструктору с параметрами
+        MyClass objectConstr1 = new MyClass(10);
+        MyClass objectConstr2 = new MyClass(88);
+
+        System.out.println(objectConstr1.x + " " + objectConstr2.x);
+    }
+}
