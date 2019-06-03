d7b8b298b0503747df17ee17a0f7ef5428a7fcd5
Hanna-Ismahilova <hi.infoshare@gmail.com>
2019-06-03 17:12:04 +0200

    minmax

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/classes/arrays/MinMax.java
@@ -0,0 +1,14 @@
+package com.mycompany.mavenproject1.classes.arrays;
+
+//Нахождение минимального и максимального значений в массиве
+
+public class MinMax {
+    public static void main(String[] args) {
+        int nums[] = new int[10];
+        int min, max;
+
+        nums[0] = 99;
+
+
+    }
+}
